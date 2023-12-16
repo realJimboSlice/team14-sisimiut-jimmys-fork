a7e36da4d5ef02cf82a2911688ee887006381bbe
realJimboSlice <jimm7493@stud.kea.dk>
2023-12-16 18:58:31 +0100

    Fixed errors, deleted slideshow_about.js, updated slideshow.js

--- src/scripts/slideshow.js
+++ src/scripts/slideshow.js
@@ -1,78 +1,76 @@
-if (typeof window !== "undefined") {
-  let slideIndex = 0;
-  let timer;
-  let startX = 0;
-  let endX = 0;
+let slideIndex = 0;
+let timer;
+let startX = 0;
+let endX = 0;
 
-  // The function that starts the slideshow
-  function startSlideshow() {
-    timer = setTimeout(showSlides, 5000);
-  }
+// The function that starts the slideshow
+function startSlideshow() {
+  timer = setTimeout(showSlides, 5000);
+}
 
-  // The function that shows the slides
-  function showSlides() {
-    let i;
-    const slides = document.getElementsByClassName("mySlides");
-    const dots = document.getElementsByClassName("dot");
-
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-      slideIndex = 1;
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-
-    startSlideshow();
-  }
-
-  // Pauses the slideshow when hovering over dots
-  const dots = document.getElementsByClassName("dot");
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].addEventListener("mouseover", () => {
-      clearTimeout(timer);
-    });
-    dots[i].addEventListener("mouseout", () => {
-      startSlideshow();
-    });
-  }
-
-  // Pauses the slideshow when hovering over the image
+// The function that shows the slides
+function showSlides() {
+  let i;
   const slides = document.getElementsByClassName("mySlides");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].addEventListener("mouseover", () => {
-      clearTimeout(timer);
-    });
-    slides[i].addEventListener("mouseout", () => {
-      startSlideshow();
-    });
-    slides[i].addEventListener("click", () => {
-      clearTimeout(timer);
-    });
-  }
+  const dots = document.getElementsByClassName("dot");
 
-  // Makes it possible to click on the dots
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].addEventListener("click", () => {
-      slideIndex = i;
-      showSlides();
-      clearTimeout(timer);
-    });
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 
-  // Displays the first image immediately on page load
-  window.onload.addEventListener("load", () => {
-    slides[0].style.display = "block";
-    dots[0].className += " active";
-    //  startSlideshow();
-    showSlides();
+  startSlideshow();
+}
+
+// Pauses the slideshow when hovering over dots
+const dots = document.getElementsByClassName("dot");
+for (let i = 0; i < dots.length; i++) {
+  dots[i].addEventListener("mouseover", () => {
+    clearTimeout(timer);
+  });
+  dots[i].addEventListener("mouseout", () => {
+    startSlideshow();
   });
 }
+
+// Pauses the slideshow when hovering over the image
+const slides = document.getElementsByClassName("mySlides");
+for (let i = 0; i < slides.length; i++) {
+  slides[i].addEventListener("mouseover", () => {
+    clearTimeout(timer);
+  });
+  slides[i].addEventListener("mouseout", () => {
+    startSlideshow();
+  });
+  slides[i].addEventListener("click", () => {
+    clearTimeout(timer);
+  });
+}
+
+// Makes it possible to click on the dots
+for (let i = 0; i < dots.length; i++) {
+  dots[i].addEventListener("click", () => {
+    slideIndex = i;
+    showSlides();
+    clearTimeout(timer);
+  });
+}
+
+// Displays the first image immediately on page load
+window.onload.addEventListener("load", () => {
+  slides[0].style.display = "block";
+  dots[0].className += " active";
+  //  startSlideshow();
+  showSlides();
+});
 
 // Adds touch functionality to the slideshow
 // Add event listeners for touch events
